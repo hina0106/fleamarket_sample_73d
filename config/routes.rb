92edf5fefc7d4932ef717f0afdb827a1216be6a0
yoshikif25 <ilk0724yf@yahoo.co.jp>
2020-07-02 21:04:29 +0900

    summary

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'items/index'
+  get 'items/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#detail'
-
   resources :items,only: [:new] 
-
 end
