4a8b204c0aec8337d768a36c31337866e5868c04
megkato <meg.8.xxx@i.softbank.jp>
2020-07-02 20:48:32 +0900

    Update ボタンフッター実装

--- db/migrate/20200630140249_create_items.rb
+++ db/migrate/20200630140249_create_items.rb
@@ -1,9 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      t.string :content
       t.string :name, null: false
-      t.text :introduction, null: false
-      t.integer :price, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_065838) do
+ActiveRecord::Schema.define(version: 2020_06_30_140249) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
     t.string "name", null: false
-    t.text "introduction", null: false
-    t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
