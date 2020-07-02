3da6185b70891542d116cbe573d373a5966612f4
takeruokutani <65336565+takeruokutani@users.noreply.github.com>
2020-07-02 21:17:49 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   get 'items/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#new"
-
   resources :items,only: [:new] 
-
 end
