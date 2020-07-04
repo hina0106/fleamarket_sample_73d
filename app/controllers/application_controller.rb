940d8918cf60ec95574adb5f68deb95fcb8361d4
hina0106 <hi0106na@gmail.com>
2020-07-04 17:39:12 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      puts ENV["BASIC_AUTH_USER"]
+      puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     end
   end
 end
