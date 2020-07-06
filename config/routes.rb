0d9a9f45c2514d027004c22a4de9c0847b7053ce
megkato <meg.8.xxx@i.softbank.jp>
2020-07-07 08:57:06 +0900

    修正
    _main.html.haml prefixに修正
    _routes.rb　get 'item/new'を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'items/index'
-  get 'items/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
   resources :items,only: [:new] 
