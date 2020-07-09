3ddea8effc5a61a9a65d3ebde03476b84e1e00c1
megkato <meg.8.xxx@i.softbank.jp>
2020-07-09 23:26:31 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage_type
   belongs_to_active_hash :prefecture_code
   belongs_to_active_hash :size
+
+  validates :name, :text, :category_id, :condition_id, :deliverycost_id, :pref_id, :delivery_days_id, :user_id, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
