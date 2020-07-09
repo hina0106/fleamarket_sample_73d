b7ab11b324b55c6e92fda1de1fab6b8981995c8b
hina0106 <hi0106na@gmail.com>
2020-07-09 21:01:21 +0900

    fix test code of sending_destination model

---
+++ spec/factories/sending_destinations.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+
+  factory :sending_destination do
+    destination_first_name        {"田中"}
+    destination_family_name       {"花子"}
+    destination_first_name_kana   {"タナカ"}
+    destination_family_name_kana  {"ハナコ"}
+    post_code                     {"0000000"}
+    prefecture_code               {"00"}
+    city                          {"テスト市"}
+    house_number                  {"0000000000"}
+  end
+
+end
---
+++ spec/models/sending_destination_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+describe SendingDestination do
+  describe '#create' do
+    # 1
+    it "destination_first_name、destination_family_name、destination_first_name_kana、destination_family_name_kana、post_code、prefecture_code、city、house_numberが存在すれば登録できること" do
+      sendingDestination = build(:sending_destination)
+      expect(sendingDestination).to be_valid
+    end
+
+    # 2
+    it "destination_first_nameがない場合は登録できないこと" do
+      sendingDestination = build(:sending_destination, destination_first_name: nil)
+      sendingDestination.valid?
+      expect(sendingDestination.errors[:destination_first_name]).to include("can't be blank")
+    end
+
+    # 3
+    it "destination_family_nameがない場合は登録できないこと" do
+      sendingDestination = build(:sending_destination, destination_family_name: nil)
+      sendingDestination.valid?
+      expect(sendingDestination.errors[:destination_family_name]).to include("can't be blank")
+    end
+
+    # 4
+    it "destination_first_name_kanaがない場合は登録できないこと" do
+      sendingDestination = build(:sending_destination, destination_first_name_kana: nil)
+      sendingDestination.valid?
+      expect(sendingDestination.errors[:destination_first_name_kana]).to include("can't be blank")
+    end
+
+    # 5
+    it "destination_family_name_kanaがない場合は登録できないこと" do
+      sendingDestination = build(:sending_destination, destination_family_name_kana: nil)
+      sendingDestination.valid?
+      expect(sendingDestination.errors[:destination_family_name_kana]).to include("can't be blank")
+    end
+
+    # 6
+    it "post_codeがない場合は登録できないこと" do
+      sendingDestination = build(:sending_destination, post_code: nil)
+      sendingDestination.valid?
+      expect(sendingDestination.errors[:post_code]).to include("can't be blank")
+    end
+
+    # 7
+    it "prefecture_codeがない場合は登録できないこと" do
+      sendingDestination = build(:sending_destination, prefecture_code: nil)
+      sendingDestination.valid?
+      expect(sendingDestination.errors[:prefecture_code]).to include("can't be blank")
+    end
+
+    # 8
+    it "cityがない場合は登録できないこと" do
+      sendingDestination = build(:sending_destination, city: nil)
+      sendingDestination.valid?
+      expect(sendingDestination.errors[:city]).to include("can't be blank")
+    end
+
+    # 9
+    it "house_numberがない場合は登録できないこと" do
+      sendingDestination = build(:sending_destination, house_number: nil)
+      sendingDestination.valid?
+      expect(sendingDestination.errors[:house_number]).to include("can't be blank")
+    end
+
+  end
+end
