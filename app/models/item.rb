60107c469ac6170610bbe056a4e978071282e5d7
megkato <meg.8.xxx@i.softbank.jp>
2020-07-09 23:35:09 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,21 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture_code
   belongs_to_active_hash :size
 
+  validate :itemimages_presence
   validates :name, :text, :category_id, :condition_id, :deliverycost_id, :pref_id, :delivery_days_id, :user_id, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  has_many_attached :images
+
+   #imageのバリデーション
+   def itemimages_presence
+    if images.attached?
+      # inputに保持されているimagesがあるかを確認
+      if images.length > 10
+        errors.add(:image, '10枚まで投稿できます')
+      end
+    else
+      errors.add(:image, '画像がありません')
+    end
+  end
 end
