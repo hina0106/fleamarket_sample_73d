778bdf248441678d702063576077e455b78f8674
megkato <meg.8.xxx@i.softbank.jp>
2020-07-09 23:15:28 +0900

    Update active_hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :item_condition
   belongs_to_active_hash :postage_payer
   belongs_to_active_hash :postage_type
+  belongs_to_active_hash :prefecture_code
+  belongs_to_active_hash :size
 end
