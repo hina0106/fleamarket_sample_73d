2f47152b08e25f73a324f1720177f32f05e46244
hina0106 <hi0106na@gmail.com>
2020-07-11 22:22:44 +0900

    add autmatic address input function

--- app/models/sending_destination.rb
+++ app/models/sending_destination.rb
@@ -1,6 +1,18 @@
 class SendingDestination < ApplicationRecord
   belongs_to :user, optional: true
 
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  # postal_codeからprefecture_nameに変換
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+    
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
   validates :destination_first_name,
             :destination_family_name,
             :destination_first_name_kana,
