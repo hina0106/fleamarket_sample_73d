7d4532bc0a2ef161239743fcc9121a2766d34adf
hina0106 <hi0106na@gmail.com>
2020-07-12 10:41:10 +0900

    used faker-japanese

--- spec/factories/sending_destinations.rb
+++ spec/factories/sending_destinations.rb
@@ -1,13 +1,16 @@
+require 'faker'
 FactoryBot.define do
 
   factory :sending_destination do
-    destination_first_name        {"田中"}
-    destination_family_name       {"花子"}
-    destination_first_name_kana   {"タナカ"}
-    destination_family_name_kana  {"ハナコ"}
+    Faker::Config.locale = :ja
+
+    destination_first_name        {Faker::Japanese::Name.first_name}
+    destination_family_name       {Faker::Japanese::Name.last_name}
+    destination_first_name_kana   {Faker::Japanese::Name.first_name.yomi}
+    destination_family_name_kana  {Faker::Japanese::Name.last_name.yomi}
     post_code                     {"0000000"}
     prefecture_code               {"00"}
-    city                          {"テスト市"}
+    city                          {Faker::Address.city}
     house_number                  {"0000000000"}
     user
   end
