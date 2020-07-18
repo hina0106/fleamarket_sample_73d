11c79da06b3d83fb5008c062bcce8c9f9c90ce47
hina0106 <hi0106na@gmail.com>
2020-07-18 12:37:33 +0900

    add test code of #create

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Users::RegistrationsController do
   let(:user) { FactoryBot.create(:user) }
   let(:sending_destination) { FactoryBot.create(:sending_destination) }
-  let(:build_user) { FactoryBot.build(:user) }
 
   setup do
     request.env["devise.mapping"] = Devise.mappings[:user]
@@ -17,27 +16,33 @@ describe Users::RegistrationsController do
   end
 
   describe '#create' do
-    it "@userに期待した値が入っていること" do
-      post :create, params: {user:  build_user.attributes}
-      expect(assigns(:user)) == build_user
+    context 'バリデーションチェックに成功した場合' do
+      let(:build_user) { FactoryBot.build(:user) }
+      it "@userに期待した値が入っていること" do
+        post :create, params: {user:  build_user.attributes}
+        expect(assigns(:user)) == build_user
+      end
+      it "newSendingDestination.html.erbに遷移すること" do
+        get :newSendingDestination
+        expect(response).to render_template :newSendingDestination
+      end
     end
-    it "@userに期待した値が入っていない場合create.html.hamlにリダイレクトすること" do
-    end
-    it "@sendingDestinationに期待した値が入っていること" do
-    end
-    it "newSendingDestination.html.erbに遷移すること" do
-      get :newSendingDestination
-      expect(response).to render_template :newSendingDestination
+    context 'バリデーションチェックに失敗した場合' do
+      let(:invalid_params) { { user: attributes_for(:user, email: nil) } }
+      it "new.html.hamlに戻ること" do
+        post :create, params: {user:  invalid_params}
+        expect(response).to render_template :new
+      end
     end
   end
 
   describe '#createSendingDestination' do
-    context '保存に成功した場合' do
-      # let(:params) { { user_id: user.id, sending_destination: attributes_for(:sending_destination) } }
-      # subject {
-      #   post :createSendingDestination,
-      #   params: params
-      # }
+    context 'バリデーションチェックに成功した場合' do
+      let(:params) { { user_id: user.id, sending_destination: attributes_for(:sending_destination) } }
+      subject {
+        post :createSendingDestination,
+        params: params
+      }
       it "userを保存すること" do
         # expect{ subject }.to change(User, :count).by(1)
       end
@@ -49,12 +54,12 @@ describe Users::RegistrationsController do
         # expect(response).to render_template :newSendingDestination
       end  
     end
-    context '保存に失敗した場合' do
+    context 'バリデーションチェックに失敗した場合' do
       it "userを保存しないこと" do
       end
       it "sendingDestinationを保存しないこと" do
       end
-      it "sending_destinations_pathにリダイレクトすること" do
+      it "newSendingDestination.html.erbに遷移すること" do
       end  
     end
   end
