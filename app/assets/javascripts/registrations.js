fac5f151c1ce1beea579d3b6835f1fb68fa12334
hina0106 <hi0106na@gmail.com>
2020-07-21 22:19:23 +0900

    add layout of signin

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -4,9 +4,9 @@ $( function() {
       '#postcode'
     ],
     address : {
-      '#sending_destination_prefecture_code' : "%3", //都道府県が入力される
-      '#address_city'                        : "%4%5", //市区町村と町域が入力される
-      '#address_street'                      : "%6%7" //大口事務所の番地と名称が入力される
+      '#address_prefecture_code'   : "%3", //都道府県が入力される
+      '#address_city'              : "%4%5", //市区町村と町域が入力される
+      '#address_street'            : "%6%7" //大口事務所の番地と名称が入力される
     }
   });
 });
