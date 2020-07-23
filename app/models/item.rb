85830acb02d54ee11709d1d682385f8e2309bbbb
hina0106 <hi0106na@gmail.com>
2020-07-24 00:33:23 +0900

    add size, item_condition, postage_payer, preparation_day,  postage_type

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   belongs_to :brand
   has_many :item_img, dependent: :destroy
   belongs_to :category
+  belongs_to_active_hash :size
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :postage_type
+  belongs_to_active_hash :preparation_day
 end
