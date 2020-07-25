0349727ff9414dd12f2066f986420e3cec779a9f
hina0106 <hi0106na@gmail.com>
2020-07-25 09:08:34 +0900

    add function of put up multiple images

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,31 @@
+$(document).on('turbolinks:load', ()=> {
+  console.log("OK");
+  // 画像用のinputを生成する関数
+  const buildFileField = (index)=> {
+    const html = `<div data-index="${index}" class="js-file_group">
+                    <input class="js-file" type="file"
+                    name="product[images_attributes][${index}][url]"
+                    id="product_images_attributes_${index}_url"><br>
+                    <div class="js-remove">削除</div>
+                  </div>`;
+    return html;
+  }
+
+  // file_fieldのnameに動的なindexをつける為の配列
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+
+  $('#image-input').on('change', '.js-file', function(e) {
+    console.log("OK");
+    // fileIndexの先頭の数字を使ってinputを作る
+    $('#image-input').append(buildFileField(fileIndex[0]));
+    fileIndex.shift();
+    // 末尾の数に1足した数を追加する
+    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+  });
+
+  $('#image-input').on('click', '.js-remove', function() {
+    $(this).parent().remove();
+    // 画像入力欄が0個にならないようにしておく
+    if ($('.js-file').length == 0) $('#image-input').append(buildFileField(fileIndex[0]));
+  });
+});
