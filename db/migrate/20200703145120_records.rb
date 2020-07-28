2c43bb58bc1f0c8e4c6d97524809fcecf99a5d0e
hina0106 <hi0106na@gmail.com>
2020-07-28 23:19:30 +0900

    remove ancestry to brands

--- db/migrate/20200703145120_records.rb
+++
@@ -1,3 +0,0 @@
-class Records < ActiveRecord::Migration[5.2]
-  change_column_default :items, :trading_status, from: nil, to: 0
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_111347) do
+ActiveRecord::Schema.define(version: 2020_07_28_140949) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
