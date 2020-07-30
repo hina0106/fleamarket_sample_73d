dbb34b22d620498a0f8b562c4af28e07b506b3cd
t-11 <mcz.rekaashiamo@gmail.com>
2020-07-30 21:11:32 +0900

    表示失敗

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,82 @@
 class ItemsController < ApplicationController
+  before_action :set_item,    only: [:show,:destroy,:edit,:update]
+  before_action :set_category, except:[:show,:destroy,:index]
+  before_action :move_to_sign_in,except: [:index,:show]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+    @item.images.build
+    def get_category_child
+      @category_child = Category.find(params[:parent_id]).children
+      render json: @category_child
+    end
+  
+    def get_category_grandchild
+      @category_grandchild = Category.find(params[:child_id]).children
+      render json: @category_grandchild
+    end
+  end
+
+  def create
+    @item = Item.new(item_params) 
+    if @item.save
+      redirect_to root_path
+    else 
+      redirect_to new_item_path
+    end
+  end
+
+  def edit
+    def get_category_child
+      @category_child = Category.find(params[:parent_id]).children
+      render json: @category_child
+    end
+  
+    def get_category_grandchild
+      @category_grandchild = Category.find(params[:child_id]).children
+      render json: @category_grandchild
+    end
+  end
+
+  def show
+    @prefectures = Prefecture.all
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path, notice: '変更しました！'
+   else
+    redirect_to edit_item_path(@item)
+   end
+  end
+
+  def destroy
+    if @item.destroy 
+       redirect_to root_path, notice: '削除しました！'
+    else
+      render :show
+    end
+  end
+
+ private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name,:price,:introduction,:bland,:prefecture_name,:condition_id,
+    :postage_payer,:preparation_day,:category_id,images_attributes:[:url,:_destroy, :id]).merge(seller_id:current_user.id)
+  end
+
+  def set_category
+    @category_parent = Category.where(ancestry: nil)
+  end
+
+  def move_to_sign_in
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,32 @@ Rails.application.routes.draw do
     post 'sending_destinations', to: 'users/registrations#createSendingDestination'
   end
   root "items#index"
+  resources :items do
+    resources :purchases, only: [:index,:new] do
+      collection do
+        get 'done', to: 'purchases#done'
+        post 'pay', to: 'purchases#pay'
+      end
+    end
+    collection do
+      get 'get_category_child', to: 'items#get_category_child', defaults: { format: 'json' }
+      get 'get_category_grandchild', to: 'items#get_category_grandchild', defaults: { format: 'json' }
+    end
+    member do
+      get 'get_category_child', to: 'items#get_category_child', defaults: { format: 'json' }
+      get 'get_category_grandchild', to: 'items#get_category_grandchild', defaults: { format: 'json' }
+  end
+end
+resources :purchases,only:[:new]
+  resources :buyers, only: [:index]
+  resources :cards,only:[:new,:create]
+  resources :mypages_cards,only:[:index,:new]
+  resources :deliveries,only:[:new,:create]
+  resources :logout,only:[:index]
+  resources :users,only:[:show]
+  resources :cards, only: [:new, :show, :destroy] do
+    collection do
+      post 'pay', to: 'cards#pay'
+    end
+  end
 end
