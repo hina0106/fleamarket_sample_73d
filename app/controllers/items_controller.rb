d0ac7d77d824c1b4244691578d7bfe6635d621b9
yoshikif25 <ilk0724yf@yahoo.co.jp>
2020-08-01 22:01:52 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,9 +33,79 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    grandchild = @item.category
+    child = grandchild.parent
+    if @category_id == 46 or @category_id == 74 or @category_id == 134 or @category_id == 142 or @category_id == 147 or @category_id == 150 or @category_id == 158
+    else
+     @parent_array = []
+     @parent_array << @item.category.parent.parent.name
+     @parent_array << @item.category.parent.parent.id
+    end
+     @category_children_array = Category.where(ancestry: child.ancestry)
+     @child_array = []
+     @child_array << child.name
+     @child_array << child.id
+
+     @category_grandchildren_array = Category.where(ancestry: grandchild.ancestry)
+     @grandchild_array = []
+     @grandchild_array << grandchild.name
+     @grandchild_array << grandchild.id
+
+  end
+
+  def update
+    if item_params[:images_attributes].nil?
+      flash.now[:alert] = '更新できませんでした 【画像を１枚以上入れてください】'
+      render :edit
+    else
+      exit_ids = []
+      item_params[:images_attributes].each do |a,b|
+        exit_ids << item_params[:images_attributes].dig(:"#{a}",:id).to_i
+      end
+      ids = Image.where(item_id: params[:id]).map{|image| image.id }
+      delete__db = ids - exit_ids
+      Image.where(id:delete__db).destroy_all
+      @item.touch
+      if @item.update(item_params)
+        redirect_to  update_done_items_path
+      else
+        flash.now[:alert] = '更新できませんでした'
+        render :edit
+      end
+    end
+  end
+
+  def update_done
+    @item_update = Item.order("updated_at DESC").first
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :prefecture_code, :brand_id, :pref_id, :size_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :postage_type_id, :category_id, :trading_status, item_imgs_attributes: [:url, :id]).merge(seller_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def category_parent_array
+    @category_parent_array = Category.where(ancestry: nil).each do |parent|
+    end
+  end
+
+  def show_all_instance
+    @user = User.find(@item.user_id)
+    @images = Image.where(item_id: params[:id])
+    @images_first = Image.where(item_id: params[:id]).first
+    @category_id = @item.category_id
+    @category_parent = Category.find(@category_id).parent.parent
+    @category_child = Category.find(@category_id).parent
+    @category_grandchild = Category.find(@category_id)
+  end
+
+end
+
 end
