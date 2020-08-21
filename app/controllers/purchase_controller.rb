8532ff35ff5b3d3405a4f198dc0ccc919bd09096
hina0106 <hi0106na@gmail.com>
2020-08-22 07:50:05 +0900

    add item purchase function

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -9,6 +9,13 @@ class PurchaseController < CardsController
   end
 
   def pay
-    
+    card = CreditCard.where(user_id: current_user.id).first
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+    Payjp::Charge.create(
+      amount: params[:item_price], #支払金額
+      customer: card.customer_id, #顧客ID
+      currency: 'jpy' #日本円
+    )
+    Item.where(id: params[:item_id]).update(buyer_id: current_user.id, trading_status: 1)
   end
 end
