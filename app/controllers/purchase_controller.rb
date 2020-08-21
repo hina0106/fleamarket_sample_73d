34011af6d40498997864c6fa5c9fd1d0973d44a5
hina0106 <hi0106na@gmail.com>
2020-08-21 18:40:18 +0900

    modified purchase page

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,9 +1,10 @@
 class PurchaseController < CardsController
   alias :super_show :show
 
-  def index
+  def show
     super_show()
     @send_info = SendingDestination.where(user_id: current_user.id)
-    pref = JpPrefecture::Prefecture.find 13
+    @pref = JpPrefecture::Prefecture.find @send_info[0].prefecture_code
+    @item = Item.find(params[:id])
   end
 end
