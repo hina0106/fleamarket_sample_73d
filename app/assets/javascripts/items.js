71ec135dd34c652b3fd3b21904e5e75f3f062ee3
hina0106 <hi0106na@gmail.com>
2020-08-29 21:43:51 +0900

    modified

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -44,7 +44,7 @@ $(document).on('turbolinks:load', ()=> {
     // 末尾の数に1足した数を追加する
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
     const url = location.href
-    if (url.endsWith('/items/new')) {
+    if (url.endsWith('/items/new') || url.endsWith('/items')) {
       $('#image-input').before(newBuildImg(targetIndex, blobUrl));
     } else {
       $('#image-input').before(buildImg(targetIndex, blobUrl));
