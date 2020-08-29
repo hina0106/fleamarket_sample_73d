30c2d38bf928944182d6a7622850dcd9f0fa3bb6
hina0106 <hi0106na@gmail.com>
2020-08-29 18:10:47 +0900

    add error handling

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -10,12 +10,15 @@ class PurchaseController < CardsController
 
   def pay
     card = CreditCard.where(user_id: current_user.id).first
-    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
-    Payjp::Charge.create(
-      amount: params[:item_price], #支払金額
-      customer: card.customer_id, #顧客ID
-      currency: 'jpy' #日本円
-    )
-    Item.where(id: params[:item_id]).update(buyer_id: current_user.id, trading_status: 1)
+    unless card.blank?
+      Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+      Payjp::Charge.create(
+        amount: params[:item_price], #支払金額
+        customer: card.customer_id, #顧客ID
+        currency: 'jpy' #日本円
+      )
+      Item.where(id: params[:item_id]).update(buyer_id: current_user.id, trading_status: 1)
+    end
+    redirect_to new_card_path
   end
 end
