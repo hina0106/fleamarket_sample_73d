38baa16f57bb40335a6134a7339608d95d656b00
hina0106 <hi0106na@gmail.com>
2020-08-29 14:30:54 +0900

    ability to delete images

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,26 +1,36 @@
 $(document).on('turbolinks:load', ()=> {
   // 画像用のinputを生成する関数
   const buildFileField = (index)=> {
-    const html = `<input accept="image/*" class="js-file" data-index="${index}" style="display: none;", type="file" name="item[item_imgs_attributes][${index}][url]" id="item_item_imgs_attributes_${index}_url">`;
+    const html = `<div data-index="${index}" class="js-file_group">
+                    <input accept="image/*" class="js-file" data-index="${index}" style="display: none;" type="file"
+                    name="item[item_imgs_attributes][${index}][url]"
+                    id="item_item_imgs_attributes_${index}_url">
+                  </div>`;
     return html;
   }
   // プレビュー用のimgタグを生成する関数
   const buildImg = (index, url)=> {
-    const html = `<div class="delete"><img data-index="${index}" src="${url}" width="100px" height="100px">
-                  <div class="imgdelete">削除</div></div>`;
+    const html = `<div data-index="${index}" class="js-file_group">
+                    <img data-index="${index}" src="${url}" width="100px" height="100px">
+                    <div class="js-remove">削除</div>
+                  </div>`;
     return html;
   }
-  $(document).on("click", '.imgdelete', function(){
-    //プレビュー要素を取得
-    var target_image = $(this).parent()
-    //プレビューを削除
-    target_image.remove();
-    //inputタグに入ったファイルを削除
-    file_field.val("")
-  })
+  // $(document).on("click", '.imgdelete', function(){
+  //   //プレビュー要素を取得
+  //   var target_image = $(this).parent()
+  //   //プレビューを削除
+  //   target_image.remove();
+  //   //inputタグに入ったファイルを削除
+  //   file_field.val("")
+  // })
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  // 既に使われているindexを除外
+  lastIndex = $('.js-file_group:last').data('index');
+  fileIndex.splice(0, lastIndex);
+  $('.hidden-destroy').hide();
 
   $('#image-input').on('change', '.js-file', function(e) {
     // labelタグのfor属性を変更
@@ -36,13 +46,27 @@ $(document).on('turbolinks:load', ()=> {
     const blobUrl = window.URL.createObjectURL(file);
     $('#image-input').before(buildImg(targetIndex, blobUrl));
   });
+
+  $('.output-box').on('click', '.js-remove', function(){
+    const targetIndex = $(this).parent().data('index')
+    console.log(targetIndex);
+    // 該当indexを振られているチェックボックスを取得する
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    // もしチェックボックスが存在すればチェックを入れる
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
+
+    $(this).parent().remove();
+    $(`img[data-index="${targetIndex}"]`).remove();
+  });
 });
 
-$(document).on("click", '.sell-main__delete-image', function(){
-  //プレビュー要素を取得
-  var target_image = $(this).parent().parent()
-  //プレビューを削除
-  target_image.remove();
-  //inputタグに入ったファイルを削除
-  file_field.val("")
-})
+
+
+// $(document).on("click", '.sell-main__delete-image', function(){
+//   //プレビュー要素を取得
+//   var target_image = $(this).parent().parent()
+//   //プレビューを削除
+//   target_image.remove();
+//   //inputタグに入ったファイルを削除
+//   file_field.val("")
+// })
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -79,7 +79,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :prefecture_code, :brand_id, :pref_id, :size_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :postage_type_id, :category_id, :trading_status, item_imgs_attributes: [:url, :id]).merge(seller_id: current_user.id)
+    params.require(:item).permit(:name, :introduction, :price, :prefecture_code, :brand_id, :pref_id, :size_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :postage_type_id, :category_id, :trading_status, item_imgs_attributes: [:url, :_destroy, :id]).merge(seller_id: current_user.id)
   end
 
   def set_categories
