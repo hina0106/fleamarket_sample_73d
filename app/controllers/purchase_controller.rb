d6fc9366aa7397f07f074eb48204ed9851cfbedf
hina0106 <hi0106na@gmail.com>
2020-08-30 09:35:08 +0900

    modified

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -10,7 +10,9 @@ class PurchaseController < CardsController
 
   def pay
     card = CreditCard.where(user_id: current_user.id).first
-    unless card.blank?
+    if card.blank?
+      redirect_to new_card_path
+    else
       Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
       Payjp::Charge.create(
         amount: params[:item_price], #支払金額
@@ -19,6 +21,5 @@ class PurchaseController < CardsController
       )
       Item.where(id: params[:item_id]).update(buyer_id: current_user.id, trading_status: 1)
     end
-    redirect_to new_card_path
   end
 end
